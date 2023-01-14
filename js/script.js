440a48cc5f88bfb1164d742be6404c74a91ab608
Monika Pflegel <monikapflegel@gmail.com>
2023-01-14 13:10:34 +0100

    script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("Cześć!");
+let button = document.querySelector(".button");
+let body = document.querySelector(".body");
+let button__pagecolor = document.querySelector(".button__pagecolor");
+button.addEventListener("click", () => {
+    body.classList.toggle("dark"); button__pagecolor.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
+})
